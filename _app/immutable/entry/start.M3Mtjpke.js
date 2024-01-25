fcaa73ae72793c90a3330612190537ad08f9413a
Hababi <76075036+GitHababi@users.noreply.github.com>
2024-01-24 18:34:35 -0800

    Revert "Hababisoft site v2"
    This reverts commit 70e1d99146100e93189c78853fc540d24d9544ae.

--- _app/immutable/entry/start.M3Mtjpke.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.sul9R0DJ.js";export{t as start};
--- _app/immutable/nodes/2.tKsnNgOt.js
+++
@@ -1,6 +0,0 @@
-import{s as c,n as o}from"../chunks/scheduler.kAlY8CUs.js";import{S as h,i as v,e as u,s as y,c as f,g as p,a as b,b as g,d as i,f as r}from"../chunks/index.CEoBq9mR.js";function _(d){let e,l="Hi!",a,s,m=`I make software whenever I feel like it. You can you use it, as long as you give me credit. (If you do actually find a use for the stuff I&#39;ve made please let me know.) I am <b>not</b> responsible if you manage to mess up your computer with my programs. Most of the software I provide is free and licensed under GPLv3, so use it at your own risk. Regardless, please enjoy this new version of my landing page.
-    <br/><br/>
-    Regards,
-    <br/><br/>
-    Hababi
-    <br/><br/>`;return{c(){e=u("h1"),e.textContent=l,a=y(),s=u("p"),s.innerHTML=m,this.h()},l(t){e=f(t,"H1",{"data-svelte-h":!0}),p(e)!=="svelte-3kvvl0"&&(e.textContent=l),a=b(t),s=f(t,"P",{style:!0,"data-svelte-h":!0}),p(s)!=="svelte-a2a71y"&&(s.innerHTML=m),this.h()},h(){g(s,"max-width","900px")},m(t,n){i(t,e,n),i(t,a,n),i(t,s,n)},p:o,i:o,o,d(t){t&&(r(e),r(a),r(s))}}}class I extends h{constructor(e){super(),v(this,e,null,_,c,{})}}export{I as component};
