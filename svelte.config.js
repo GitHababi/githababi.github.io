0131b39e4e295083bce3456d0c6ed27d846a9297
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2024-01-24 22:40:31 -0800

    this should fix

--- svelte.config.js
+++ svelte.config.js
@@ -10,13 +10,7 @@ const config = {
 	kit: {
 		adapter: adapter({
 			fallback: '404.html'
-		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : /** @type {`/${string}`} */ (process.env.BASE_PATH)
-		},
-		prerender: {
-			// entries: []
-		}
+		})
 	}
 };
 
